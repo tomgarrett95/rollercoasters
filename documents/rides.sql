744102942d18eae92f8514645c4a2c909bc0b860
Tom <tgarrett.tech@gmail.com>
2021-09-27 14:13:07 +0100

    solved sql error

--- documents/rides.sql
+++ documents/rides.sql
@@ -17,19 +17,16 @@ CREATE TABLE `rides` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(255) DEFAULT NULL,
   `location` int(11) unsigned DEFAULT NULL,
-  `height` int(11) unsigned DEFAULT NULL,
+  `img` varchar(255) DEFAULT NULL,
+  `height` float DEFAULT NULL,
   `inversions` int(11) unsigned DEFAULT NULL,
   PRIMARY KEY (`id`),
-  CONSTRAINT `fk_rides_locations` FOREIGN KEY (`location`) REFERENCES `themeparks`(`id`),
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
+  CONSTRAINT `fk_rides_locations` FOREIGN KEY (`location`) REFERENCES `themeparks`(`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
 
-INSERT INTO `themeparks` (`id`, `name`, `location`, `height`, `inversions`) VALUES (1, 'Stealth', 1, 1);
-INSERT INTO `themeparks` (`id`, `name`, `location`, `height`, `inversions`) VALUES (2, 'Colossus', 1, 4);
-INSERT INTO `themeparks` (`id`, `name`, `location`, `height`, `inversions`) VALUES (3, 'Nemesis', 2, 2);
-INSERT INTO `themeparks` (`id`, `name`, `location`, `height`, `inversions`) VALUES (4, 'The Smiler', 2, 2);
-INSERT INTO `themeparks` (`id`, `name`, `location`, `height`, `inversions`) VALUES (5, 'Black Mamba', 3, 2);
-INSERT INTO `themeparks` (`id`, `name`, `location`, `height`, `inversions`) VALUES (6, 'Wild West Mine Train', 4, 21, );
-
-
-
-
+INSERT INTO `rides` (`id`, `name`, `location`, `img`, `height`, `inversions`) VALUES (1, 'Stealth', 1, 'stealth.jpg', 63, 0);
+INSERT INTO `rides` (`id`, `name`, `location`, `img`, `height`, `inversions`) VALUES (2, 'Colossus', 1, 'colossus.png', 30, 10);
+INSERT INTO `rides` (`id`, `name`, `location`, `img`, `height`, `inversions`) VALUES (3, 'Nemesis', 2, 'nemisis.jpg', 13, 4);
+INSERT INTO `rides` (`id`, `name`, `location`, `img`, `height`, `inversions`) VALUES (4, 'The Smiler', 2, 'the-smiler.jpg', 30, 14);
+INSERT INTO `rides` (`id`, `name`, `location`, `img`, `height`, `inversions`) VALUES (5, 'Black Mamba', 3, 'black-mamba.jpg', 26, 4);
+INSERT INTO `rides` (`id`, `name`, `location`, `img`, `height`, `inversions`) VALUES (6, 'Wild West Mine Train', 4, 'wild-west-mine-train.jpg', 21, 0);
